25f00f8e9c271fe53bc6cde8f31ae5a18af39fd8
ishtiyaque.jaffer <ishtiyaque.jaffer@iiflcapital.com>
2025-02-19 17:34:07 +0530

    dadasd

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ connectDB().catch((err) => {
     process.exit(1);
 });
 
-const authRoutes = require("./routes/auth");
+// const authRoutes = require("./routes/auth");
 // const userRoutes = require("./routes/user");
 // const productRoutes = require("./routes/product");
 // const cartRoutes = require("./routes/cart");
@@ -31,7 +31,7 @@ app.get("/", (req, res) => {
     res.send("HomeBudget Server is up and running");
 });
 
-app.use("/api/auth", authRoutes);
+// app.use("/api/auth", authRoutes);
 // app.use("/api/users", userRoutes);
 // app.use("/api/products", productRoutes);
 // app.use("/api/cart", cartRoutes);
