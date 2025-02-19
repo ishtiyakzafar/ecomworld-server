2acafcb7f936eff98414a416710548c2c54c3f98
ishtiyaque.jaffer <ishtiyaque.jaffer@iiflcapital.com>
2025-02-19 18:08:49 +0530

    dsssda

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user');
-const { verifyAccessToken, requireLogin, userMiddleware } = require('../middleware');
+// const { verifyAccessToken, requireLogin, userMiddleware } = require('../middleware');
 
 // router.get('/profile', userController.getUserProfile);
 router.get('/', userController.getAllUsers);
-router.get('/:id', userController.getUserById);
-router.put('/:id', userController.updateUserByAdmin);
-router.delete('/:id', userController.deleteUser);
-router.post('/reset-password', requireLogin, verifyAccessToken, userMiddleware, userController.resetPassword);
-router.post('/update', requireLogin, userMiddleware, userController.updateDetailByCustomer);
+// router.get('/:id', userController.getUserById);
+// router.put('/:id', userController.updateUserByAdmin);
+// router.delete('/:id', userController.deleteUser);
+// router.post('/reset-password', requireLogin, verifyAccessToken, userMiddleware, userController.resetPassword);
+// router.post('/update', requireLogin, userMiddleware, userController.updateDetailByCustomer);
 
 module.exports = router;
